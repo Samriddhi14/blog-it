057d21646e65985401cc3b4e1e725d34c9491537
Samriddhi14 <sam14.diva@gmail.com>
2025-01-07 13:45:40 +0530

    feat: added models

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -6,6 +6,32 @@ const Component = styled(Box)`
     width: 400px;
     margin: auto;
     box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+    padding: 50px;
+`;
+const Image = styled('img')({ //image treated as variable
+    width: 200,
+    display: 'flex',
+    margin: 'auto',
+    padding: '0 50px 0',
+    borderRadius: '30% 0% 30% 0%'
+  });
+
+  const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 20px;
+    }
+`;
+  const LoginButton = styled(Button)`
+    text-transform: none;
+    background: #3E5879 ;
+    color: #fff;
+    height: 48px;
+    border-radius: 2px;
 `;
 
 const Login = () => {
@@ -13,12 +39,15 @@ const Login = () => {
 
     return (
         <Component> 
-            <img src = {imageURL} alt = "login" /> <br></br>
-            <TextField variant='standard' />
-            <TextField variant='standard' /> 
-            <br></br>
-            <Button variant='contained'>Login</Button>
-            <Button> Create an Account</Button>
+            <Image src = {imageURL} alt = "login" />
+            <Wrapper>
+                <TextField variant='standard' />
+                <TextField variant='standard' /> 
+            </Wrapper>
+            <Wrapper>
+                <LoginButton variant='contained'>Login</LoginButton>
+                <Button> Create an Account</Button>
+            </Wrapper>
         </Component>
         
     )
