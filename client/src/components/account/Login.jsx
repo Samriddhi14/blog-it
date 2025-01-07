012dc29d362568b33eadd8f46d3856051822b104
Samriddhi14 <sam14.diva@gmail.com>
2025-01-07 13:13:12 +0530

    chore: login page update 2

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,18 +1,25 @@
 
-import { TextField, Box, Button } from '@mui/material';
+import { TextField, Box, Button, styled} from '@mui/material';
 import loginImage from '../account/logo.png';
+
+const Component = styled(Box)`
+    width: 400px;
+    margin: auto;
+    box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`;
 
 const Login = () => {
     const imageURL = loginImage;
 
     return (
-        <Box> 
-            <img src = {imageURL} alt = "login" />
+        <Component> 
+            <img src = {imageURL} alt = "login" /> <br></br>
             <TextField variant='standard' />
-            <TextField variant='standard' />
+            <TextField variant='standard' /> 
+            <br></br>
             <Button variant='contained'>Login</Button>
-            <Button> Create Account</Button>
-        </Box>
+            <Button> Create an Account</Button>
+        </Component>
         
     )
 }
