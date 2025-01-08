2b5ea424ca666f9660b125f8014cc466ac160d23
Samriddhi14 <sam14.diva@gmail.com>
2025-01-08 19:15:03 +0530

    fix: fixed runtime errors

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,13 +1,13 @@
-import User from "../model/schema.js";
-
+import Schema from "../model/schema.js";
+ 
 export const signupUser = async (request, response) => {
     try{
         const user = request.body;
-
-        const newUser = new User(user);
+        const newUser = new Schema.User(user);
         await newUser.save();
         return response.status(200).json({msg: 'Signup successful'})
     }catch(error){
+        console.log(error);
         return response.status(500).json({msg: 'Error while signing up'})
     }
 }
