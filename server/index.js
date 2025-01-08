231b9ca6bff050c0900047e986bf4f4d1115f438
Samriddhi14 <sam14.diva@gmail.com>
2025-01-08 12:48:09 +0530

    chore: backend setup complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import Connection from "./database/db.js";
+import Router from "./routes/route.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
+
+app.use('/', Router);
 
 const PORT = 8000;
 
